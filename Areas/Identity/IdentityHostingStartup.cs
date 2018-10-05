cdc075b6d608f46b5e5a8de5d3e616f1d5bb5f88
Tony Morris <anthony.frederick.morris@gmail.com>
2018-10-05 19:36:02 -0400

    Add the rest of the files

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SO_52674151.Data;
+
+[assembly: HostingStartup(typeof(SO52674151.Areas.Identity.IdentityHostingStartup))]
+namespace SO52674151.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
